63f30f9b0e3f619bd04c2136ee0dac8b591a49a7
Matthew Booe <mabooe@microsoft.com>
2020-09-28 17:30:12 -0700

    Improve get examples, fix test and error handling

--- src/azure-cli/azure/cli/command_modules/find/_example_service.py
+++ src/azure-cli/azure/cli/command_modules/find/_example_service.py
@@ -2,10 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from collections import namedtuple
 import re
 import json
 
+from collections import namedtuple
 
 from azure.cli.command_modules.find._aladdin_service import call_aladdin_service
 
